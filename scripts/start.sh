8de57ca19452cad3e7c72f959b058fe0ffaae195
YIBO0408 <1730859967@qq.com>
2024-03-29 14:45:57 +0800

    update shell docker run

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+parent_dir=$(dirname "$(pwd)")
+
 docker run \
 --privileged \
 --gpus all \
 --rm \
 -it \
--v /home/yibo/yolov8_cpp/YOLOv8-ONNXRuntime-CPP:/home/yibo/yolov8_cpp/YOLOv8-ONNXRuntime-CPP \
--w /home/yibo/yolov8_cpp/YOLOv8-ONNXRuntime-CPP/scripts \
+-v $parent_dir:$parent_dir \
+-w $parent_dir/scripts \
 hub.micro-i.com.cn:9443/dad/tritonserver_dev:23.09-py3 bash
